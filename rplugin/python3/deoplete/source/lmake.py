7a2211e083de0232c5d9967ac2cb019a8def3622
Haotian Wang <twistoy.wang@gmail.com>
2018-11-23 14:11:41 +0800

    Add deoplete support

---
+++ rplugin/python3/deoplete/source/lmake.py
@@ -0,0 +1,22 @@
+from .base import Base
+
+
+CompleteOutputs = "g:LanguageClient_omniCompleteResults"
+
+
+class Source(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.name = "LMAKE"
+        self.mark = "[LM]"
+        self.rank = 1000
+        self.min_pattern_length = 1
+        self.filetypes = ['bzl']
+        self.input_pattern += r'(:)\w*$'
+
+    def gather_candidates(self, context):
+        hints = self.vim.call('_lbuild_get_hints')
+        return hints
+
+
